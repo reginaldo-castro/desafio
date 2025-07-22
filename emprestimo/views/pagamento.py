fabfb0ead01be738537e487acee10b2fb30c27d1
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-07-22 15:40:05 -0400

    Atualizando a view pagamanto com restrição de usuario cadastrado

--- emprestimo/views/pagamento.py
+++ emprestimo/views/pagamento.py
@@ -1,13 +1,20 @@
-from emprestimo.serializers.pagamento import PagamentoListSerializer
+from emprestimo.serializers.pagamento import PagamentoSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from emprestimo.models.pagamento import Pagamento
+from rest_framework.exceptions import ValidationError
 
 class PagamantoViewSet(viewsets.ModelViewSet):
     
     permission_classes = [ IsAuthenticated]
-    serializer_class = PagamentoListSerializer
+    serializer_class = PagamentoSerializer
     
     def get_queryset(self):
         return Pagamento.objects.filter(emprestimo__usuario=self.request.user)
     
+    def perform_create(self, serializer):
+        
+        emprestimo = serializer.validated_data["emprestimo"]
+        if emprestimo.usuario != self.request.user:
+            raise ValidationError("Empréstimo não encontrado")
+        serializer.save()

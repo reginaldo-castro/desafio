dbc86fbc7a582a5701b1d4f051b09d9667e27bb1
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-07-21 23:15:10 -0400

    Adicionando o painel admin pagamento e emprestimo

--- emprestimo/admin.py
+++ emprestimo/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from emprestimo.models.emprestimo import Emprestimo
+from emprestimo.models.pagamento import Pagamento
 
-admin.site.register(Emprestimo)
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'valor_nominal', 'taxa_juros', 'data_solicitacao', 'banco', 'usuario', 'ip_cadastro']
+    list_filter = ['banco', 'data_solicitacao', 'usuario']
+    search_fields = ['cliente', 'banco', 'id']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    
+    
+@admin.register(Pagamento)
+class PagamentoEmprestimoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'emprestimo', 'data_pagamento', 'valor_pagamento']
+    list_filter = ['emprestimo', 'data_pagamento']
+    search_fields = ['cliente', 'banco', 'id']
+    readonly_fields = ['id', 'created_at', 'updated_at']

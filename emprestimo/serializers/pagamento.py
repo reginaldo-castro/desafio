fa77b220a28ef6781eb3d4e7bc3e84d8ea20dbcc
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-07-22 13:51:59 -0400

    Criando validacoes pagamento

--- emprestimo/serializers/pagamento.py
+++ emprestimo/serializers/pagamento.py
@@ -10,4 +10,21 @@ class PagamentoListSerializer(serializers.ModelSerializer):
         fields = ["id", "emprestimo", "data_pagamento", "valor_pagamento", "created_at"]
         ready_only_fields = ["id", "created_at"]
         
+    def validate_valor_pagamento(self, value):
         
+        if value <= 0:
+            raise serializers.ValidationError("Valor do pagamento deve ser positivo")
+        return value
+    
+    def validate(self, data):
+        
+        emprestimo = data.get("emprestimo")
+        data_pagamento = data.get("data_pagamento")
+        
+        if emprestimo and data_pagamento:
+            if data_pagamento < emprestimo.data_solicitacao:
+                raise serializers.ValidateError(
+                    "Data de pagamento não pode ser anterior à data de solicitação"
+                )
+                
+        return data

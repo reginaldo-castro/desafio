fa7ee902d33b6801be934922a683391bc742ecdf
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-07-23 07:59:32 -0400

    Correção da url de geração do token

--- gerenciador_emprestimo/urls.py
+++ gerenciador_emprestimo/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('emprestimo.urls')),
-    path('api-token-auth', views.obtain_auth_token, name="api_token_auth"),
+    path('api-token-auth/', views.obtain_auth_token, name="api_token_auth"),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
